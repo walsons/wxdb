2a55c9081106257510069db9185259717d6d831e
walsons <walsons@163.com>
2021-10-26 23:35:41 +0800

    main loop

--- src/main.cc
+++ src/main.cc
@@ -1,28 +1,45 @@
 #include <iostream>
-#include "meta_commands.h"
+#include "meta_command.h"
+#include "sql_statement.h"
 
 int main() {
-    MetaCommands meta_commands;
+    MetaCommand meta_command;
     std::cout << "wxdb version 1.0" << std::endl;
     std::cout << "Enter \".help\" for usage hints." << std::endl;
     std::cout << "Connected to a transient in-memory database." << std::endl;
     std::cout << "Use \".open FILENAME\" to reopen on a persistent database." << std::endl;
-    bool flag = true;
-    while (flag)
+    bool main_loop_flag = true;
+    while (main_loop_flag)
     {
         std::cout << "wxdb> " << std::flush;
         std::string user_input;
-        std::cin >> user_input;
-        if (meta_commands.command2number(user_input) == -1) {
-            std::cout << "unknown command" << std::endl;
-            continue;
-        }
-        switch(meta_commands.command2number(user_input)) {
-            case 0: 
-                flag = false;
-                break;
-            default:
-                break;
+        std::getline(std::cin, user_input);
+        
+        if (user_input[0] == '.') {
+            // Meta commands
+            if (!meta_command.parse(user_input)) {
+                main_loop_flag = false;
+            }
+        } else {
+            // SQL statements
+            StatementType statement_type = OTHER_TYPE;
+            if (user_input.substr(0, 6) == "insert") {
+                statement_type = INSERT_TYPE;
+            } else if (user_input.substr(0, 6) == "select") {
+                statement_type = SELECT_TYPE;
+            }
+            switch (statement_type) {
+                case INSERT_TYPE:
+                    std::cout << "do insert" << std::endl;
+                    break;
+                case SELECT_TYPE:
+                    std::cout << "do select" << std::endl;
+                    break;
+                default:
+                    std::cout << "unknown command" << std::endl;
+                    break;
+            }
+            
         }
     }
     
---
+++ src/sql_statement.h
@@ -0,0 +1,10 @@
+#ifndef SQL_STATEMENT_H_
+#define SQL_STATEMENT_H_
+
+typedef enum {
+    OTHER_TYPE,
+    INSERT_TYPE,
+    SELECT_TYPE
+} StatementType;
+
+#endif
