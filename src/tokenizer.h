1912a0af9e84415a8705f0e0726867ae363ec436
walsons <walsons@163.com>
2022-04-07 09:50:23 +0800

    Add tokenizer (#11)
    * add tokenizer

---
+++ src/tokenizer.h
@@ -0,0 +1,49 @@
+// lexical analysis
+
+#ifndef TOKENIZER_H_
+#define TOKENIZER_H_
+
+#include "token.h"
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+class Tokenizer
+{
+public:
+    Tokenizer(std::string statement);
+    ~Tokenizer();
+
+    bool NextChar();
+    void ClearBuffer();
+    Token *MakeToken(Token_Type token_type);
+    Token *GetNextToken();
+
+private:
+    std::string input_stream_;
+    std::string::size_type input_iter_;
+    std::string token_buffer_;
+    std::string::size_type buffer_iter_;
+    Token_Type curr_token_type_;
+    std::unordered_map<std::string, bool> reserved_word_;
+
+private:
+    bool IsReservedWord(std::string word);
+    Token *word();
+    Token *zero();
+    Token *octal();
+    Token *hex(bool is_x);
+    Token *fraction(bool is_dot);
+    Token *decimal();
+    Token *not_equal();
+    Token *double_quote();
+
+    Token *open_parenthesis();
+    Token *close_parenthesis();
+
+    Token *semicolon();
+
+    Token *invalid();
+};
+
+#endif
---
+++ unit_test/database_test.cc
@@ -0,0 +1,57 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch2.hpp"
+
+#include <string>
+#include <vector>
+
+#include "../src/meta_command.h"
+#include "../src/sql_statement.h"
+
+MetaCommand meta_command;
+TEST_CASE( "META_COMMAND", "[meta command test]" ) {
+    meta_command.ExecuteMetaCommand(".unrecognized command such as fjslkjkef");
+    meta_command.ExecuteMetaCommand(".exit");
+    CHECK(meta_command.MainLoop() == false);
+    meta_command.ExecuteMetaCommand(".help");
+};
+
+SQLStatement sql_statement;
+Database database;
+TEST_CASE( "SQL_STATEMENT", "[SQL statement test]" ) {
+    std::string user_input = "create user";
+    PREPARE_RESULT res = sql_statement.PrepareStatement(database, user_input);
+    REQUIRE(res == PREPARE_RESULT::SUCCESS);
+    sql_statement.ExecuteStatement(database, user_input);
+
+    user_input = "select user"; 
+    res = sql_statement.PrepareStatement(database, user_input);
+    REQUIRE(res == PREPARE_RESULT::SUCCESS);
+    sql_statement.ExecuteStatement(database, user_input);
+
+    std::vector<int> ids{1,5,4,8,7,2,3,9,12,16,23,21,20,6,10,22,19,11,18,14,13,15,17,28};
+    for (const auto &id : ids)
+    {
+        user_input = "insert user " + std::to_string(id) +  " walsons walsons@163.com";
+        res = sql_statement.PrepareStatement(database, user_input);
+        REQUIRE(res == PREPARE_RESULT::SUCCESS);
+        sql_statement.ExecuteStatement(database, user_input);
+    }
+
+    user_input = "select user"; 
+    res = sql_statement.PrepareStatement(database, user_input);
+    REQUIRE(res == PREPARE_RESULT::SUCCESS);
+    sql_statement.ExecuteStatement(database, user_input);
+
+    // Print tree structure
+    Pager *pager = database.table()->pager;
+    unsigned root_page_num = database.table()->root_page_num;
+    if (pager->GetPageType(root_page_num) == NODE_TYPE::INTERNAL_NODE)
+    {
+
+    }
+    else
+    {
+        LeafNode *leaf_node = pager->GetLeafNode(root_page_num);
+        std::cout << CELL_CAPACITY << std::endl;
+    }
+};
