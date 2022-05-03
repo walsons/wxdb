91ce31421a0fe6182fdc6c2227b8c562a1687f3b
walsons <walsons@163.com>
2022-05-03 17:12:27 +0800

    add index_leaf_page

---
+++ include/page/index_leaf_page.h
@@ -0,0 +1,29 @@
+#ifndef INDEX_LEAF_PAGE_H_
+#define INDEX_LEAF_PAGE_H_
+
+#include "fixed_page.h"
+
+template <typename T>
+class IndexLeafPage : public FixedPage
+{
+public:
+    using FixedPage<T>::FixedPage;
+    void Init(int field_size);
+    std::pair<int, IndexLeafPage> Split(int current_id);
+};
+
+template <typename T > inline
+void IndexLeafPage<T>::Init(int field_size)
+{
+    FixedPage<T>::Init(field_size);
+    page_type() = Page_Type::INDEX_LEAF_PAGE;
+}
+
+template <typename T > inline
+std::pair<int, IndexLeafPage<T>> IndexLeafPage<T>::Split(int current_id)
+{
+    auto res = FixedPage<T>::Split(current_id);
+    return { res.first, *reinterpret_cast<IndexLeafPage *>(&res.second)};
+}
+
+#endif
