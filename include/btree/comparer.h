fae96a4701b9db2b1a9ee1248d365b33934fe2e7
walsons <walsons@163.com>
2022-05-06 18:10:42 +0800

    add comparer and IntBTree

---
+++ include/btree/comparer.h
@@ -0,0 +1,38 @@
+#ifndef COMPARER_H_
+#define COMPARER_H_
+
+#include <cstring>
+
+template <typename T>
+inline int basic_type_comparer(const T &x, const T &y)
+{
+    if (x < y) return -1;
+    return x == y ? 0 : 1;
+}
+
+inline int integer_comparer(const int &x, const int &y)
+{
+    return basic_type_comparer(x, y);
+}
+
+inline int float_comparer(const float &x, const float &y)
+{
+    return basic_type_comparer(x, y);
+}
+
+inline int integer_cast_comparer(const char *x, const char *y)
+{
+    return basic_type_comparer(*reinterpret_cast<const int *>(x), *reinterpret_cast<const int *>(y));
+}
+
+inline int float_cast_comparer(const char *x, const char *y)
+{
+    return basic_type_comparer(*reinterpret_cast<const float *>(x), *reinterpret_cast<const float *>(y));
+}
+
+inline int string_comparer(const char *x, const char *y)
+{
+    return std::strcmp(x, y);
+}
+
+#endif
