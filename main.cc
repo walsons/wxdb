cbdc28d7e2cb206c4cb0afc29b92e10fd70872ba
walsons <walsons@163.com>
2022-06-05 14:38:39 +0800

    delete the old implementation

--- main.cc
+++ main.cc
@@ -1,45 +1,4 @@
-/******************** old implement ********************
-#include <iostream>
-#include "include/db_old/hello_interface.h"
-#include "include/db_old/meta_command.h"
-#include "include/db_old/a_sql_statement.h"
-
-int main() 
-{
-    HelloInterface();
-    MetaCommand metaCommand;
-    ASQLStatement sqlStatement;
-    Database database;
-    while (metaCommand.MainLoop())
-    {
-        std::cout << "wxdb> " << std::flush;
-        std::string userInput;
-        std::getline(std::cin, userInput);
-
-        if (userInput.empty())
-        {
-            continue;   
-        }
-        else if (userInput[0] == '.') 
-        {
-            // Meta commands
-            metaCommand.ExecuteMetaCommand(userInput);
-        } 
-        else 
-        {
-            // SQL statements
-            if (sqlStatement.PrepareStatement(database, userInput) == PREPARE_RESULT::SUCCESS)
-            {
-                sqlStatement.ExecuteStatement(database, userInput);
-            }
-        }
-    }
-    
-    return 0;
-}
- ******************** old implement ********************/
-
-#include "include/db_old/hello_interface.h"
+#include "include/db/hello_interface.h"
 #include "include/main_loop.h"
 
 int main()
