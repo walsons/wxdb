42d071b29dd278ad9ea35f75ce1280199f8ef336
walsons <walsons@163.com>
2022-06-09 23:48:46 +0800

    Select using index (#97)
    * select using index
    
    * fix issue

--- include/btree/btree_iterator.hpp
+++ include/btree/btree_iterator.hpp
@@ -9,10 +9,15 @@
 template <typename PageType>
 class BTreeIterator
 {
+    int page_id_, pos_;
+    // first way to iterate via traverse
     int original_page_id_, original_pos_;
     std::shared_ptr<Pager> pg_;
-    int page_id_, pos_;
     int cur_size_, prev_page_id_, next_page_id_;
+    // another way to iterate via rowid
+    std::vector<int> rows_;
+    int rows_counter_;
+    std::shared_ptr<IntBTree> btr_;
     void load_info(int page_id)
     {
         page_id_ = page_id;
@@ -34,11 +39,37 @@ public:
         , pg_(pg), page_id_(page_id), pos_(pos) { load_info(page_id_); }
     BTreeIterator(std::shared_ptr<Pager> pg, value_t value)
         : BTreeIterator(pg, value.first, value.second) {}
-    std::shared_ptr<Pager> GetPager() { return pg_; }
-    value_t Get() { return {page_id_, pos_}; }
+    BTreeIterator(std::vector<int> rows, std::shared_ptr<IntBTree> btr) 
+        : rows_(rows), rows_counter_(0), btr_(btr) 
+    {
+        auto page_pos = btr_->upper_bound(btr_->root_page_id(), rows_[rows_counter_]);
+        page_id_ = page_pos.first;
+        pos_ = page_pos.second;
+
+    }
+    std::shared_ptr<Pager> GetPager() 
+    { 
+        // via rowid
+        if (!rows_.empty()) { return btr_->pg(); }
+        // via traverse
+        return pg_; 
+    }
+    value_t Get() 
+    { 
+        return {page_id_, pos_}; 
+    }
     value_t operator*() { return Get(); }
     value_t next()
     {
+        // via rowid
+        if (!rows_.empty())
+        {
+            auto page_pos = btr_->upper_bound(btr_->root_page_id(), rows_[rows_counter_++]);
+            page_id_ = page_pos.first;
+            pos_ = page_pos.second;
+            return Get();
+        }
+        // via traverse
         assert(page_id_);
         if (++pos_ == cur_size_)
         {
@@ -49,6 +80,15 @@ public:
     }
     value_t prev()
     {
+        // via rowid
+        if (!rows_.empty())
+        {
+            auto page_pos = btr_->upper_bound(btr_->root_page_id(), rows_[rows_counter_--]);
+            page_id_ = page_pos.first;
+            pos_ = page_pos.second;
+            return Get();
+        }
+        // via traverse
         assert(page_id_);
         if (pos_-- == 0)
         {
@@ -57,9 +97,25 @@ public:
         }
         return Get();
     }
-    bool IsEnd() { return page_id_ == 0; }
+    bool IsEnd() 
+    {
+        // via rowid
+        if (!rows_.empty())
+        {
+            return rows_counter_ == rows_.size() || rows_counter_ == -1;
+        }
+        // via traverse
+        return page_id_ == 0; 
+    }
     void Reset() 
     { 
+        // via rowid
+        if (!rows_.empty())
+        {
+            rows_counter_ = 0;
+            return;
+        }
+        // via traverse
         load_info(original_page_id_);
         pos_ = original_pos_;
     }
