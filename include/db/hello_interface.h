d948584b8bb1729d3e3cb55e2a6cd1c66c4c2600
walsons <walsons@163.com>
2022-08-05 18:32:45 +0800

    update main function

--- include/db/hello_interface.h
+++ include/db/hello_interface.h
@@ -8,8 +8,6 @@ public:
     HelloInterface() {
         std::cout << "wxdb version 1.0" << std::endl;
         std::cout << "Enter \".help\" for usage hints." << std::endl;
-        std::cout << "Connected to a transient in-memory database." << std::endl;
-        std::cout << "Use \".open FILENAME\" to reopen on a persistent database." << std::endl;
     }
     ~HelloInterface() = default;
 };
